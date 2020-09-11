4665a5189d0291dae992c1d386d74e03d3bcea3c
Cody P Schafer <dev@codyps.com>
2020-09-10 20:30:35 -0400

    zfs-core-sys: fix build on macos (openzfsonosx.org 1.9.4)

--- zfs-core-sys/build.rs
+++ zfs-core-sys/build.rs
@@ -2,7 +2,7 @@ extern crate pkg_config;
 
 fn main()
 {
-    if cfg!(target_os = "freebsd") {
+    if cfg!(target_os = "freebsd") || cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=zfs_core");
     } else {
         pkg_config::probe_library("libzfs_core").unwrap();
