da735c639fd08df9c565c38beab338471e4b5b73
Cody P Schafer <dev@codyps.com>
2020-11-24 18:00:18 -0500

    zfs-core-sys: provide new bindings generate, cleanup wrapper.h

--- zfs-core-sys/wrapper.h
+++ zfs-core-sys/wrapper.h
@@ -1,12 +1,1 @@
-#include <stdint.h>
-#include <stdarg.h>
-#include <stddef.h>
-
-typedef unsigned int uint_t;
-typedef enum {
-	B_FALSE,
-	B_TRUE,
-} boolean_t;
-typedef unsigned char uchar_t;
-typedef unsigned long hrtime_t;
 #include <libzfs_core.h>
